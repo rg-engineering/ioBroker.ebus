ba69764659b41b26cabfbe0e1f8af43bb445d85b
Rene <info@rg-engineering.eu>
2018-01-13 17:38:29 +0100

    translations

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ﻿![Logo](admin/myhomecontrol.png)
 # ioBroker.ebus
 ===========================
+
 [![NPM version](https://img.shields.io/npm/v/iobroker.ebus.svg)](https://www.npmjs.com/package/iobroker.ebus)
 [![Downloads](https://img.shields.io/npm/dm/iobroker.ebus.svg)](https://www.npmjs.com/package/iobroker.ebus)
 
@@ -16,7 +17,7 @@ supported ebusd-version: 3.1
 - web interface of Arduino based ebus adapter. Interpretation of ebus data is done on Arduino.
 details incl. Arduino software see /t.b.d./
    
-Attention: ebus-history and ebus widget works only with arduino interface at the moment. Will be implemented later.
+Attention: ebus-history and ebus widget works only with arduino interface at the moment. Will be implemented for ebusd later.
 
 ## Changelog
 
@@ -41,33 +42,14 @@ Attention: ebus-history and ebus widget works only with arduino interface at the
 * (René) initial release
 
 ## License
-Copyright (C) <2016>  <info@rg-engineering.eu>
+Copyright (C) <2018>  <info@rg-engineering.eu>
 
-//
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-//    This program is free software: you can redistribute it and/or modify
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-//    it under the terms of the GNU General Public License as published by
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-//    the Free Software Foundation, either version 3 of the License, or
-
-//    (at your option) any later version.
-
-//
-
-//    This program is distributed in the hope that it will be useful,
-
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-
-//    GNU General Public License for more details.
-
-//
-
-//    You should have received a copy of the GNU General Public License
-
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 
--- admin/words.js
+++ admin/words.js
@@ -6,7 +6,7 @@ systemDictionary = {
     {
         "en": "ebus adapter settings",
         "de": "ebus Adaptereinstellungen",
-        "ru": "????????? ???????? ebus",
+        "ru": "настройки адаптера ebus",
         "pt": "Configurações do adaptador ebus",
         "nl": "ebus-adapterinstellingen",
         "fr": "Paramètres de l'adaptateur ebus",
@@ -17,20 +17,20 @@ systemDictionary = {
     "restart_hint":
     {
         "en": "on save adapter restarts with new config immediately",
-        "de": "Beim Speichern von Einstellungen wird der Adapter sofort neu gestartet.",
-        "ru": "?? ???????????????? ???????? ??????????????? ? ????? ????????????? ??????????",
+        "de": "Beim Speichern wird der Adapter sofort mit der neuen Konfiguration neu gestartet",
+        "ru": "на перезаписываемом адаптере перезагружается с новой конфигурацией немедленно",
         "pt": "no adaptador de salvar reinicia com nova configuração imediatamente",
         "nl": "on save-adapter wordt onmiddellijk opnieuw opgestart met nieuwe config",
         "fr": "sur l'adaptateur de sauvegarde redémarre avec la nouvelle config immédiatement",
         "it": "su save adapter si riavvia immediatamente con la nuova configurazione",
         "es": "en el adaptador de guardar se reinicia con nueva configuración de inmediato",
-        "pl": "na karcie save natychmiast uruchamia si? z now? konfiguracj?"
+        "pl": "na karcie save natychmiast uruchamia się z nową konfiguracją"
     },
     "target_IP":
     {
         "en": "target IP",
         "de": "Ziel-IP",
-        "ru": "??????? IP-?????",
+        "ru": "целевой IP-адрес",
         "pt": "IP alvo",
         "nl": "doel-IP",
         "fr": "cible IP",
@@ -41,44 +41,56 @@ systemDictionary = {
     "target_Port":
     {
         "en": "target Port (ebusd only)",
-        "de": "Ziel-Port (nur ebusd)",
-        "ru": "??????? IP-?????",
-        "pt": "IP alvo",
-        "nl": "doel-IP",
-        "fr": "cible IP",
-        "it": "IP di destinazione",
-        "es": "IP objetivo",
-        "pl": "docelowy adres IP"
+        "de": "Ziel Port (nur ebusd)",
+        "ru": "целевой порт (только для ebusd)",
+        "pt": "destino Port (somente ebusd)",
+        "nl": "doelpoort (alleen ebusd)",
+        "fr": "Port cible (ebusd uniquement)",
+        "it": "Porta di destinazione (solo ebusd)",
+        "es": "puerto de destino (solo ebusd)",
+        "pl": "cel Port (tylko ebusd)"
     },
     "hint_arduino":
     {
-        "en": "still missing",
-        "de": "immer noch vermisst",
-        "ru": "?? ??? ??? ???????????",
-        "pt": "ainda desaparecido",
-        "nl": "nog steeds vermist",
-        "fr": "toujours manquant",
-        "it": "ancora disperso",
-        "es": "sigue desaparecido",
-        "pl": "nadal t?skni?"
+        "en": "for arduino interface you need arduino, ethernet shield, ebus adapter and our arduino software. Details see:",
+        "de": "für das Arduino Interface benötigen sie einen Arduino, Ethernet Shield, ebus Adapter und unsere Arduino Software. Details siehe:",
+        "ru": "для интерфейса arduino вам нужны arduino, ethernet-экран, адаптер ebus и наше программное обеспечение arduino. Подробности см .:",
+        "pt": "Para a interface arduino você precisa de arduino, ethernet shield, adaptador ebus e nosso software arduino. Detalhes:",
+        "nl": "voor arduino-interface heeft u arduino, ethernet shield, ebus-adapter en onze arduino-software nodig. Details zien:",
+        "fr": "Pour l'interface arduino, vous avez besoin d'arduino, d'un shield ethernet, d'un adaptateur ebus et de notre logiciel arduino. Détails voir:",
+        "it": "per l'interfaccia arduino hai bisogno di arduino, schermo ethernet, adattatore ebus e il nostro software arduino. Dettagli vedi:",
+        "es": "para la interfaz arduino necesitas arduino, escudo ethernet, adaptador ebus y nuestro software arduino. Los detalles ven:",
+        "pl": "do interfejsu arduino potrzebujesz arduino, ekran ethernetowy, adapter ebus i nasze oprogramowanie arduino. Szczegóły patrz:"
     },
     "hint_ebusd":
     {
-        "en": "still missing",
-        "de": "immer noch vermisst",
-        "ru": "?? ??? ??? ???????????",
-        "pt": "ainda desaparecido",
-        "nl": "nog steeds vermist",
-        "fr": "toujours manquant",
-        "it": "ancora disperso",
-        "es": "sigue desaparecido",
-        "pl": "nadal t?skni?"
+        "en": "for ebusd interface you need a running ebusd incl. all necessary configurations. This adpater uses HTML to read data from ebusd",
+        "de": "Für das ebusd Interface benötigen Sie einen laufenden ebusd incl. aller notwendigen Konfigurationen. Dieser Adpater verwendet HTML, um die Daten vom ebusd zu lesen",
+        "ru": "для интерфейса ebusd вам необходимо запустить ebusd, вкл. все необходимые конфигурации. Этот adpater использует HTML для чтения данных из ebusd",
+        "pt": "Para a interface ebusd, você precisa de um ebusd executado. todas as configurações necessárias. Este adpater usa HTML para ler dados do ebusd",
+        "nl": "voor ebusd-interface heeft u een lopende ebusd nodig. alle noodzakelijke configuraties. Deze adpater gebruikt HTML om gegevens van ebusd te lezen",
+        "fr": "Pour l'interface ebusd, vous avez besoin d'un ebusd en cours d'exécution. toutes les configurations nécessaires. Cet outil utilise le HTML pour lire les données d'ebusd",
+        "it": "per l'interfaccia ebusd hai bisogno di un ebusd in esecuzione incl. tutte le configurazioni necessarie Questo adpater utilizza l'HTML per leggere i dati da ebusd",
+        "es": "para la interfaz ebusd necesita un ebusd eb todas las configuraciones necesarias. Este adpater usa HTML para leer datos de ebusd",
+        "pl": "do interfejsu ebusd potrzebny jest działający ebusd w tym. wszystkie niezbędne konfiguracje. Ten adpater używa HTML do odczytu danych z ebusd"
+    },
+    "hint_general":
+    {
+        "en": "this adpater reads only data from ebus. At the moment writing is not implemented yet",
+        "de": "Dieser Adpater liest nur Daten von ebus. Im Moment ist das Schreiben noch nicht implementiert",
+        "ru": "этот adpater считывает только данные из ebus. На данный момент запись еще не реализована",
+        "pt": "Este adpater lê apenas dados do ebus. No momento, a escrita ainda não foi implementada.",
+        "nl": "deze adpater leest alleen gegevens van ebus. Op dit moment is schrijven nog niet geïmplementeerd",
+        "fr": "cet adpater lit uniquement les données d'ebus. Pour le moment, l'écriture n'est pas encore implémentée",
+        "it": "questo advisor legge solo i dati di ebus. Al momento la scrittura non è ancora stata implementata",
+        "es": "este adpater solo lee datos de ebus. Por el momento, la escritura aún no está implementada",
+        "pl": "ten adpater odczytuje tylko dane z ebus. W tej chwili pisanie nie jest jeszcze realizowane"
     },
     "interface_type":
     {
         "en": "type of interface",
         "de": "Art der Schnittstelle",
-        "ru": "??? ??????????",
+        "ru": "тип интерфейса",
         "pt": "tipo de interface",
         "nl": "type interface",
         "fr": "type d'interface",
